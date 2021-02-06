7f6fbaefeb0aa97bfcf8244c2dde0c57f50736f4
Ashlee Sewell <ashleesewell@usf.edu>
2021-02-05 23:57:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Author: Ashlee Sewell
+Date: 2/5/2021
+Comments: This is the program file for the Array deliverable.
+*/
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                try
+                {
+                    //manually populated array
+                    int[] numbers = { 1, 4, 6, 8, 9, 2, 22, 29, 32, 35, 37, 38, 40, 41, 43, 47, 49, 52, 54, 57, 58, 63, 67, 69, 72 };
+                    Console.WriteLine(" Here are the values of the array:");
+                    //Interate through array with foreach loop
+                    foreach (int i in numbers)
+                    //Display array values
+                    {
+
+                        Console.Write("Element Value = " + i + " ");
+
+                    }
+                }
+
+
+
+                catch
+                {
+                    Console.WriteLine(" Press any key to exit the program");
+                    Console.ReadKey(true);
+                }
+
+            }
+        }
+    }
+}
+
